ca4505baa12fce7662092bbe714cb806a6b0b775
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-10-18 18:53:56 +1300

    Renamed variables and changed var to let

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas = new fabric.Canvas('canvas',
+let canvas = new fabric.Canvas('canvas',
     {
         width: 600,
         height: 600,
@@ -7,13 +7,13 @@ var canvas = new fabric.Canvas('canvas',
 );
 
 // https://stackoverflow.com/questions/44147762/fabricjs-snap-to-grid-on-resize
-var snapSize = 50;
-var gridSize = 50;
+let snapSize = 50;
+let gridSize = 50;
 
-var currentXSize = 1;
-var currentYSize = 1;
+let currentXScale = 1;
+let currentYScale = 1;
 
-for (var i = 0; i < (600 / gridSize); i++) {
+for (let i = 0; i < (600 / gridSize); i++) {
     canvas.add(new fabric.Line([i * gridSize, 0, i * gridSize, 600], {
         stroke: '#ccc',
         selectable: false
@@ -24,7 +24,7 @@ for (var i = 0; i < (600 / gridSize); i++) {
     }))
 }
 
-var rect = new fabric.Rect({
+let rect = new fabric.Rect({
     left: gridSize * 2,
     top: gridSize * 2,
     width: gridSize,
@@ -54,8 +54,8 @@ function Snap(value) {
     return Math.round(value / snapSize) * snapSize;
 }
 
-var selectionLeft;
-var selectionTop;
+let selectionLeft;
+let selectionTop;
 
 function SnapMoving(options) {
     options.target.set({
@@ -63,7 +63,7 @@ function SnapMoving(options) {
         top: Snap(options.target.top)
     });
 
-    var reDraw = false;
+    let reDraw = false;
 
     if (selectionLeft != Snap(options.target.left)) {
         reDraw = true;
@@ -78,11 +78,7 @@ function SnapMoving(options) {
     if (reDraw) {
         // hm either move or redraw it where it is...
 
-        var objectsToRemove = canvas.getObjects().filter(function (o) {
-            if (o.get('name') === 'layeredRect') {
-                return o;
-            }
-        });
+        let objectsToRemove = canvas.getObjects().filter(e => e["name"] === 'layeredRect');
 
         objectsToRemove.forEach(function (element) {
             //console.log(`moving to new left of: ${element.left - (selectionLeft - Snap(options.target.left))} and new top of ${element.top - (selectionTop - Snap(options.target.top))}`)
@@ -96,7 +92,6 @@ function SnapMoving(options) {
         selectionTop = Snap(options.target.top)
         selectionLeft = Snap(options.target.left)
     }
-
 }
 
 function SnapScaling(event) {
@@ -189,56 +184,50 @@ function onObjectScaled(event) {
         transform
     } = event;
     const {
-        target
+        target: scaledObject
     } = transform;
 
     const anchorY = transform.originY;
     const anchorX = transform.originX;
 
-    var scaledObject = event.target;
-
-    var isShrinkingX = false;
-    var isGrowingX = false;
-    var isShrinkingY = false;
-    var isGrowingY = false;
+    let isShrinkingX = false;
+    let isGrowingX = false;
+    let isShrinkingY = false;
+    let isGrowingY = false;
 
     // update x size
-    if (scaledObject.scaleX < currentXSize) {
+    if (scaledObject.scaleX < currentXScale) {
         isShrinkingX = true;
     }
 
-    if (scaledObject.scaleX > currentXSize) {
+    if (scaledObject.scaleX > currentXScale) {
         isGrowingX = true;
     }
 
     // update y size
-    if (scaledObject.scaleY < currentYSize) {
+    if (scaledObject.scaleY < currentYScale) {
         isShrinkingY = true;
     }
 
-    if (scaledObject.scaleY > currentYSize) {
+    if (scaledObject.scaleY > currentYScale) {
         isGrowingY = true;
     }
 
     // do some work
     if (isShrinkingX || isGrowingX || isShrinkingY || isGrowingY) {
 
-        currentXSize = scaledObject.scaleX;
-        currentYSize = scaledObject.scaleY;
-
-        var objectsToRemove = canvas.getObjects().filter(function (o) {
-            if (o.get('name') === 'layeredRect') {
-                return o;
-            }
-        });
+        currentXScale = scaledObject.scaleX;
+        currentYScale = scaledObject.scaleY;
+  
+        let objectsToRemove = canvas.getObjects().filter(e => e["name"] === 'layeredRect');
 
         // clear existing tiles
         canvas.remove(...objectsToRemove);
 
-        for (let currentX = 0; currentX < currentXSize; currentX++) {
-            for (let currentY = 0; currentY < currentYSize; currentY++) {
+        for (let currentX = 0; currentX < currentXScale; currentX++) {
+            for (let currentY = 0; currentY < currentYScale; currentY++) {
 
-                var newRect = new fabric.Rect({
+                let newRect = new fabric.Rect({
                     width: gridSize * 1,
                     height: gridSize * 1,
                     fill: '#000',
@@ -294,3 +283,15 @@ function onObjectScaled(event) {
     selectionLeft = Snap(scaledObject.left)
 }
 
+// new fabric.Canvas
+// https://github.com/fmeringdal/nettu-meet/blob/e401decf0aefe069c1f72a6a41ec4cedf282f53f/frontend/src/modules/canvas/services/CanvasStateManager.ts
+// https://github.com/fmeringdal/nettu-meet/blob/e401decf0aefe069c1f72a6a41ec4cedf282f53f/frontend/src/modules/canvas/services/CanvasManager.ts
+// https://github.com/gadenbuie/xaringanExtra/blob/c74f0a365baaa9b284168fc2632679fe2e0d3e66/inst/scribble/scribble.js
+// https://github.com/danilowoz/coverify/blob/2c37ca6d430293dc2a78dd1465cc6b68882f8eb8/src/modules/Canvas/fabricUtils/index.ts
+// https://github.com/apache/openmeetings/blob/f3ca5c24394cb9fbb8e532cb36ceaa640102bb59/openmeetings-web/src/main/front/wb/src/wb.js
+// https://github.com/Canvasbird/canvasboard/blob/8cde1f5c3bce77938d53d6c1aa4e37bdffab068c/src/app/components/new-board/new-board.component.ts
+// https://github.com/tannerkrewson/drawphone/blob/ad6ffb3259c25ff46cfcd8935295a2ac761c2ca7/client/canvas.js
+// https://github.com/oppia/oppia/blob/5bb07f6b17f19cdba43d0581a844be558a546619/extensions/objects/templates/svg-editor.component.spec.ts
+// https://github.com/ObjTube/front-end-roadmap/blob/18d638411ab9b282cb1ccd352e1edb1fed2a141c/src/page/index/drawRoadmap.js
+// https://github.com/slaylines/canvas-engines-comparison/blob/bc02a4d1992c7d297d51e5a8fb8289fbb8b6129a/src/scripts/fabric.js
+// https://github.com/nhn/tui.image-editor/blob/7ef094a0a9479cd75f322b5a87e322a43f75ea6d/apps/image-editor/src/js/graphics.js
