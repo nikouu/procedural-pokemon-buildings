736fccf7f333af3bfea8fdcca4dbc2aa844ad5be
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-10-29 18:54:54 +1300

    Added file for string encoder

---
+++ js/stringEncoder.js
@@ -0,0 +1,16 @@
+// Looking to be a rough string shortener. Since the state contains only enums and ints, I won't worry about UTF concerns
+
+// perhaps use this to shorten https://stackoverflow.com/a/25963279
+export class StringEncoder {
+	constructor() {
+		window.app.stringEncoder = this;
+	}
+
+	encode(string){
+
+	}
+
+	decode(string){
+
+	}
+}
