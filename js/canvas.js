b06bb158368789c2e49e86d20ab4bd8980e929bb
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-12-29 18:44:20 +1300

    Reverted further back

--- js/canvas.js
+++ js/canvas.js
@@ -23,7 +23,6 @@ export class Canvas {
 		this.selectionCoords = new fabric.Point(this.gridSize * 2, this.gridSize * 2);
 		this.isPanning = false;
 		this.isResizing = false;
-		this.previousTouchZoomScale = 1;
 		this.touchCoords;
 		this.zoomStartScale;
 
@@ -159,23 +158,19 @@ export class Canvas {
 				}
 
 				if (e.e.touches && e.e.touches.length == 2 && !this.isResizing) {
-					let isInitialRun = false;
 					if (e.self.state == "start") {
 						this.zoomStartScale = this.fabricCanvas.getZoom();
-						isInitialRun = true;
 					}
 
 					console.log(`${e.self.state} ${this.zoomStartScale} ${e.self.scale}`)
 
 					// issue where scale will jump to 1 then back to the real value
-					if (isInitialRun || Math.abs(e.self.scale - this.previousTouchZoomScale) < 0.2) {
+					if (e.self.scale != 1) {
 						this.#setZoom(e.self.x, e.self.y, this.zoomStartScale * e.self.scale);
 					}
 
 					e.e.preventDefault();
 					e.e.stopPropagation();
-
-					this.previousTouchZoomScale = e.self.scale;
 				} else {
 					console.log("everything else")
 				}
