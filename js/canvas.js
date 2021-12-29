2b89772eed9671e96860b032728f251bdd199659
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-12-29 18:54:32 +1300

    Another revert

--- js/canvas.js
+++ js/canvas.js
@@ -22,7 +22,6 @@ export class Canvas {
 		// x,y of the selection rectangle, kept to help calculate deltas for before and after events
 		this.selectionCoords = new fabric.Point(this.gridSize * 2, this.gridSize * 2);
 		this.isPanning = false;
-		this.isResizing = false;
 		this.touchCoords;
 		this.zoomStartScale;
 
@@ -149,34 +148,34 @@ export class Canvas {
 			"object:moving": this.#onMoving.bind(this),
 			"object:scaling": this.#onScaling.bind(this),
 			'mouse:wheel': this.#onZooming.bind(this),
-			'mouse:up': () => { this.isPanning = false; this.touch = undefined; this.isResizing = false },
+			'mouse:up': () => { this.isPanning = false; this.touch = undefined },
 			'mouse:down': () => { this.isPanning = true; },
 			'mouse:move': this.#onMouseMoving.bind(this),
 			'touch:gesture': (e) => {
-				if (e.target?.name == "selectionRect") {
-					this.isResizing = true;
-				}
+				if (e.e.touches && e.e.touches.length == 2) {
+					// this.isPanning = true;
+					// var point = new fabric.Point(e.self.x, e.self.y);
+					// let zoomStartScale;
+					// if (e.self.state == "start") {
+					// 	zoomStartScale = this.fabricCanvas.getZoom();
+					// }
+					// var delta = zoomStartScale * e.self.scale;
+					// this.fabricCanvas.zoomToPoint(point, delta);
+					// this.isPanning = false;
+					//this.#onZooming(e);
 
-				if (e.e.touches && e.e.touches.length == 2 && !this.isResizing) {
-					let isInitialRun = false;
 					if (e.self.state == "start") {
-						this.zoomStartScale = this.fabricCanvas.getZoom();
-						isInitialRun = true;
+						this.zoomStartScale = canvas.getZoom();
 					}
 
-					console.log(`${e.self.state} ${this.zoomStartScale} ${e.self.scale}`)
-
-					// issue where scale will jump to 1 then back to the real value
-					if (isInitialRun || e.self.scale != 1) {
-						this.#setZoom(e.self.x, e.self.y, this.zoomStartScale * e.self.scale);
-					}
-
+					this.#setZoom(e.self.x, e.self.y, this.zoomStartScale * e.self.scale);
 					e.e.preventDefault();
 					e.e.stopPropagation();
-				} else {
-					console.log("everything else")
 				}
 			}
+			// 'touch:orientation': (e) => {
+			// 	console.log(e);
+			// }
 		});
 	}
 
@@ -276,23 +275,22 @@ export class Canvas {
 				if (dist.height < this.gridSize) {
 					attrs.scaleY = snap.height / scaledObject.height;
 				}
+
 				break;
 			case 'mt':
 			case 'mb':
 				if (dist.height < this.gridSize) {
 					attrs.scaleY = snap.height / scaledObject.height;
 				}
+
 				break;
 			case 'ml':
 			case 'mr':
 				if (dist.width < this.gridSize) {
 					attrs.scaleX = snap.width / scaledObject.width;
 				}
+
 				break;
-			case false:
-				// difficult to get pinch for resizing working
-				// transform.corner = false when gesture resizing
-				return;
 		}
 
 		let updateScale = true;
@@ -315,6 +313,11 @@ export class Canvas {
 			this.currentXScale = attrs.scaleX;
 			this.currentYScale = attrs.scaleY;
 		}
+
+		let objectsToRemove = this.fabricCanvas.getObjects().filter(e => e.get("name") === 'buildingTile');
+
+		// clear existing tiles
+		this.fabricCanvas.remove(...objectsToRemove);
 
 		// correctly sets the values of the new selection shape size for the delta calculations in the movement
 		this.selectionCoords = new fabric.Point(this.#snap(scaledObject.left), this.#snap(scaledObject.top));
@@ -354,11 +357,11 @@ export class Canvas {
 	}
 
 	#setZoom(x, y, zoom) {
-		if (zoom > 1.5) {
-			zoom = 1.5;
+		if (zoom > 20) {
+			zoom = 20;
 		}
-		if (zoom < 0.1) {
-			zoom = 0.1;
+		if (zoom < 0.01) {
+			zoom = 0.01;
 		}
 		this.fabricCanvas.zoomToPoint({ x: x, y: y }, zoom);
 	}
